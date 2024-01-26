6f6aadeefe563d984723d9aa76fdc501b14b910a
Mark Erikson <mark@isquaredsoftware.com>
2024-01-25 22:45:58 -0500

    Add a users slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import postsReducer from '@/features/posts/postsSlice'
+import usersReducer from '@/features/users/usersSlice'
 
 export const store = configureStore({
   // Pass in the root reducer setup as the `reducer` argument
   reducer: {
     posts: postsReducer,
+    users: usersReducer,
   },
 })
 
