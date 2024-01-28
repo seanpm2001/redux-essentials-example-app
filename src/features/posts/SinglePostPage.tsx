75e70e383ba476b1b0a01cddf2014c0647a0cea6
Mark Erikson <mark@isquaredsoftware.com>
2024-01-28 12:30:57 -0500

    Extract post selectors
    # Conflicts:
    #	src/features/posts/EditPostForm.tsx
    #	src/features/posts/SinglePostPage.tsx

--- src/features/posts/SinglePostPage.tsx
+++ src/features/posts/SinglePostPage.tsx
@@ -5,11 +5,12 @@ import { TimeAgo } from '@/components/TimeAgo'
 
 import { PostAuthor } from './PostAuthor'
 import { ReactionButtons } from './ReactionButtons'
+import { selectPostById } from './postsSlice'
 
 export const SinglePostPage = () => {
   const { postId } = useParams()
 
-  const post = useAppSelector((state) => state.posts.find((post) => post.id === postId))
+  const post = useAppSelector((state) => selectPostById(state, postId!))
   const user = useAppSelector((state) => state.auth.username)
 
   if (!post) {
