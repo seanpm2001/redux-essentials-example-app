15897c3394e835df28415ec59c1727186511a961
Mark Erikson <mark@isquaredsoftware.com>
2024-02-18 16:19:07 -0500

    Use logged-in user in AddPostForm
    # Conflicts:
    #	src/features/posts/AddPostForm.tsx
    
    # Conflicts:
    #	src/features/posts/AddPostForm.tsx

--- src/features/posts/AddPostForm.tsx
+++ src/features/posts/AddPostForm.tsx
@@ -9,7 +9,6 @@ import { postAdded } from './postsSlice'
 interface AddPostFormFields extends HTMLFormControlsCollection {
   postTitle: HTMLInputElement
   postContent: HTMLTextAreaElement
-  postAuthor: HTMLSelectElement
 }
 interface AddPostFormElements extends HTMLFormElement {
   readonly elements: AddPostFormFields
@@ -17,7 +16,7 @@ interface AddPostFormElements extends HTMLFormElement {
 
 export const AddPostForm = () => {
   const dispatch = useAppDispatch()
-  const users = useAppSelector((state) => state.users)
+  const userId = useAppSelector((state) => state.auth.username)!
 
   const handleSubmit = (e: React.FormEvent<AddPostFormElements>) => {
     // Prevent server submission
@@ -26,18 +25,11 @@ export const AddPostForm = () => {
     const { elements } = e.currentTarget
     const title = elements.postTitle.value
     const content = elements.postContent.value
-    const userId = elements.postAuthor.value
 
     dispatch(postAdded(title, content, userId))
 
     e.currentTarget.reset()
   }
-
-  const usersOptions = users.map((user) => (
-    <option key={user.id} value={user.id}>
-      {user.name}
-    </option>
-  ))
 
   return (
     <section>
@@ -45,11 +37,6 @@ export const AddPostForm = () => {
       <form onSubmit={handleSubmit}>
         <label htmlFor="postTitle">Post Title:</label>
         <input type="text" id="postTitle" defaultValue="" required />
-        <label htmlFor="postAuthor">Author:</label>
-        <select id="postAuthor" name="postAuthor" required>
-          <option value=""></option>
-          {usersOptions}
-        </select>
         <label htmlFor="postContent">Content:</label>
         <textarea id="postContent" name="postContent" defaultValue="" required />
         <button>Save Post</button>
