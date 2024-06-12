1c4e2a25fad215acaf8f46f0271c5f70daa2ba13
Mark Erikson <mark@isquaredsoftware.com>
2024-06-11 22:50:21 -0400

    Change postsSlice to create new posts via API call instead of local changes

--- src/features/posts/postsSlice.ts
+++ src/features/posts/postsSlice.ts
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice, nanoid, PayloadAction } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { client } from '@/api/client'
 
 import type { RootState } from '@/app/store'
@@ -25,19 +25,12 @@ export interface Post {
 }
 
 type PostUpdate = Pick<Post, 'id' | 'title' | 'content'>
+type NewPost = Pick<Post, 'title' | 'content' | 'user'>
 
 interface PostsState {
   posts: Post[]
   status: 'idle' | 'pending' | 'failed' | 'succeeded'
   error: string | null
-}
-
-const initialReactions: Reactions = {
-  thumbsUp: 0,
-  tada: 0,
-  heart: 0,
-  rocket: 0,
-  eyes: 0,
 }
 
 export const fetchPosts = createAsyncThunk<Post[], void, { state: RootState }>(
@@ -56,6 +49,11 @@ export const fetchPosts = createAsyncThunk<Post[], void, { state: RootState }>(
   },
 )
 
+export const addNewPost = createAsyncThunk('posts/addNewPost', async (initialPost: NewPost) => {
+  const response = await client.post<Post>('/fakeApi/posts', initialPost)
+  return response.data
+})
+
 const initialState: PostsState = {
   posts: [],
   status: 'idle',
@@ -66,23 +64,6 @@ const postsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
-    postAdded: {
-      reducer(state, action: PayloadAction<Post>) {
-        state.posts.push(action.payload)
-      },
-      prepare(title: string, content: string, userId: string) {
-        return {
-          payload: {
-            id: nanoid(),
-            date: new Date().toISOString(),
-            title,
-            content,
-            user: userId,
-            reactions: initialReactions,
-          },
-        }
-      },
-    },
     postUpdated(state, action: PayloadAction<PostUpdate>) {
       const { id, title, content } = action.payload
       const existingPost = state.posts.find((post) => post.id === id)
@@ -110,17 +91,20 @@ const postsSlice = createSlice({
       })
       .addCase(fetchPosts.fulfilled, (state, action) => {
         state.status = 'succeeded'
-        // Add any fetched posts to the array
-        state.posts.push(...action.payload)
+        // Save the fetched posts into state
+        state.posts = action.payload
       })
       .addCase(fetchPosts.rejected, (state, action) => {
         state.status = 'failed'
-        state.error = action.error.message ?? 'Unknown Error'
+        state.error = action.error.message ?? null
+      })
+      .addCase(addNewPost.fulfilled, (state, action) => {
+        state.posts.push(action.payload)
       })
   },
 })
 
-export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
+export const { postUpdated, reactionAdded } = postsSlice.actions
 
 export default postsSlice.reducer
 
